281f1bcfd3ec7c02d262566c7d628926a4aa9e9b
Clarence Nguyen <clarrynguyen@gmail.com>
2016-03-15 17:19:17 -0700

    refactor

--- crawler.py
+++ crawler.py
@@ -5,42 +5,39 @@ urlLinks = []
 urlBody = []
 
 def redditTitleScrape(subreddit):
-    print "hey1"
     url = "https://www.reddit.com/r/" + subreddit
     handle = urllib.urlopen(url)
     html_feed = handle.read()
     soup = BeautifulSoup(html_feed,'html.parser')
     index = 0;
+    time.sleep(2)
     for header in soup.findAll("a",{ "class" : "title" }):
-        # time.sleep(5)
         if "None" not in header.string:
-            # time.sleep(5)
-            urlLinks.append(header.get("href"))
-            # redditBodyScrape(header.get("href"))
+            urlLinks.append(header.get("href")) # gets all the post links
             print "(" + str(index) + ") " + (header.string)  
             index+=1
 
 def redditBodyScrape(link):
-    print "hey"
     url = "https://www.reddit.com" + link
     handle = urllib.urlopen(url)
     html_feed = handle.read()
     soup = BeautifulSoup(html_feed,"html.parser")
     userText = soup.findAll("div", { "class" : "expando"})
+    time.sleep(2)
     for body in userText: 
         if "None" not in body.text:
             urlBody.append(body.text)
             print body.text
 
-def redditCommentScrape(soup):
-    print "hey2"
-    url = "https://www.reddit.com" + soup
+def redditCommentScrape(link):
+    url = "https://www.reddit.com" + link
     handle = urllib.urlopen(url)
     html_feed = handle.read()
-    print html_feed
+    # print html_feed
     soup = BeautifulSoup(html_feed,"html.parser")   
     commentSection = soup.find("div", {"class" : "commentarea"})
     commentSection = commentSection.find_all('p')
+    time.sleep(2)
     for comment in commentSection:
         if "None" not in comment.text:
             print comment.text
