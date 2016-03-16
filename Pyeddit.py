f049dfadb5bbe19530fee286fd7c0169ff315efe
Clarence Nguyen <clarrynguyen@gmail.com>
2016-03-16 16:40:28 -0700

    added a bot using praw

---
+++ Pyeddit.py
@@ -0,0 +1,43 @@
+import praw
+import sys
+import pprint
+import time
+
+
+class RedditPrawler(object):
+
+    def __init__(self):
+        self.favoriteSubReddits = ["cscareerquestions",
+                                   "blackpeopletwitter", "DBZDokkanBattle"]
+        self.r = praw.Reddit(
+            user_agent="testing_personal_reddit_crawler_donfistme")
+        self.feed = []
+
+    # Gets and sets feed and displays
+    def getSubmissions(self, choice, lim):
+        self.feed = self.r.get_subreddit(
+            self.favoriteSubReddits[int(choice)]).get_hot(limit=int(lim))
+        self.displayFeed()
+
+    def displayFeed(self):
+        print '\n'
+        for post in self.feed:
+            print ' '.join((str(post)[5:int(len(str(post)))]).split()) + '\n'
+
+    def addSubReddit(self, subreddit):
+        self.favoriteSubReddits.append(subreddit)
+        print "Successfully added : " + subreddit
+
+    def main(self):
+        print "Which subreddit would you like to go to?"
+        i = 0
+        for x in self.favoriteSubReddits:
+            print "(" + str(i) + ") " + x
+            i += 1
+        input = raw_input()
+        print "Okay going to " + self.favoriteSubReddits[int(input)]
+        self.getSubmissions(input, 10)
+
+
+pr = RedditPrawler()
+pr.main()
--- crawler.py
+++ crawler.py
@@ -1,46 +1,67 @@
 from bs4 import BeautifulSoup
-import urllib
+import requests
 import time
-urlLinks = []
-urlBody = []
+import sys
 
-def redditTitleScrape(subreddit):
-    url = "https://www.reddit.com/r/" + subreddit
-    handle = urllib.urlopen(url)
-    html_feed = handle.read()
-    soup = BeautifulSoup(html_feed,'html.parser')
-    index = 0;
-    time.sleep(2)
-    for header in soup.findAll("a",{ "class" : "title" }):
-        if "None" not in header.string:
-            urlLinks.append(header.get("href")) # gets all the post links
-            print "(" + str(index) + ") " + (header.string)  
-            index+=1
 
-def redditBodyScrape(link):
-    url = "https://www.reddit.com" + link
-    handle = urllib.urlopen(url)
-    html_feed = handle.read()
-    soup = BeautifulSoup(html_feed,"html.parser")
-    userText = soup.findAll("div", { "class" : "expando"})
-    time.sleep(2)
-    for body in userText: 
-        if "None" not in body.text:
-            urlBody.append(body.text)
-            print body.text
+class RedditCrawler(object):
+    # url = "https://www.reddit.com/r/"
+    url = "https://www.google.com/"
+    favoriteSubReddits = ["cscareerquestions",
+                          "blackpeopletwitter", "DBZDokkanBattle"]
 
-def redditCommentScrape(link):
-    url = "https://www.reddit.com" + link
-    handle = urllib.urlopen(url)
-    html_feed = handle.read()
-    # print html_feed
-    soup = BeautifulSoup(html_feed,"html.parser")   
-    commentSection = soup.find("div", {"class" : "commentarea"})
-    commentSection = commentSection.find_all('p')
-    time.sleep(2)
-    for comment in commentSection:
-        if "None" not in comment.text:
-            print comment.text
+    # Initialize the subreddit reddit Bot
+    def __init__(self, reddit):
+        self.sentRequest = False
+        # url = "https://www.reddit.com/r/"
+        self.urlLinks = []
+        self.url = "https://www.google.com/"
+        self.subreddit = self.url
+        self.html = requests.get(self.url)
+        self.soup = BeautifulSoup(self.html.text, "html.parser")
+        if self.html.status_code == 200:
+            print "Initialization sucessful ! : " + self.subreddit
+        else:
+            print "Error : " + self.html.status_code
+
+    # Scrapes all the titles of all the posts on the subreddit
+    def subRedditScrape(self):
+        index = 0
+        time.sleep(2)
+        links = self.soup.findAll("a", {"class": "title"})
+        for header in links:
+            if "None" not in header.string:
+                # gets all the post links
+                self.urlLinks.append(header.get("href"))
+                print "(" + str(index) + ") " + (header.string)
+                index += 1
+
+    # Scrapes the body of the post
+    def redditBodyScrape(self):
+        userText = self.soup.findAll("div", {"class": "expando"})
+        for body in userText:
+            if "None" not in body.text:
+                urlBody.append(body.text)
+                print body.text
+
+    # Scrapes the comments of the post
+    def redditCommentScrape(self):
+        commentSection = self.soup.find("div", {"class": "commentarea"})
+        commentSection = commentSection.find_all('p')
+        for comment in commentSection:
+            if "None" not in comment.text:
+                print comment.text
+
+    def timeLimit(self):
+        if self.sentRequest == True:
+            time.sleep(2)
+
+    def main(self):
+        self.subRedditScrape()  # Displays all posts
+        print "Which post would you like to see? . . ."
+        input = raw_input()
+        time.sleep(2)
+        print "Okay, switching to : " + input
 
 
 # redditTitleScrape('cscareerquestions')
@@ -49,4 +70,7 @@ def redditCommentScrape(link):
     # redditBodyScrape(link)
 # redditTitleScrape('DBZDokkanBattle')
 # redditBodyScrape("/r/cscareerquestions/comments/4aicx6/how_do_you_find_the_time_to_program_in_your_free/")
-redditCommentScrape("/r/cscareerquestions/comments/4aicx6/how_do_you_find_the_time_to_program_in_your_free/") 
+# redditCommentScrape(
+    # "/r/cscareerquestions/comments/4aicx6/how_do_you_find_the_time_to_program_in_your_free/")
+redditScrap = RedditCrawler("DBZDokkanBattle")
+redditScrap.main()
