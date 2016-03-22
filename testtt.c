0f5832b5b1508573194edb20e3d593c3f4410dc9
Clarence Nguyen <clarrynguyen@gmail.com>
2016-03-22 13:12:12 -0700

    removed garbage

--- testtt.c
+++
@@ -1,6 +0,0 @@
-int main(int argc, char *argv[])
-{
-    // ... // draw our borders
-    draw_borders(field); draw_borders(score);
-    // simulate the game loop 
-    while(1) { // draw to our windows mvwprintw(field, 1, 1, "Field"); mvwprintw(score, 1, 1, "Score"); // refresh each window wrefresh(field); wrefresh(score); } // clean up delwin(field); delwin(score); // ... }
